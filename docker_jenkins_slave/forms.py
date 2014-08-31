451d394f1709777d611cd50c7b65351e4acd672b
Roman Rader <roman.rader@gmail.com>
2014-08-31 15:41:33 +0300

    form and styles

--- docker_jenkins_slave/forms.py
+++ docker_jenkins_slave/forms.py
@@ -4,6 +4,15 @@ class DockerfileRequestForm(forms.Form):
     OSs = (
         ('centos6', 'CentOS 6'),
         ('centos5', 'CentOS 5'),
-        ('centos5', 'SUSE')
+        ('suse12', 'SUSE 12'),
+        ('debian6', 'Debian 6 Squeeze'),
         )
-    os = forms.ChoiceField(label='OS', choices=OSs)
+    os = forms.ChoiceField(label='OS', choices=OSs, initial='centos6')
+    ssh = forms.BooleanField(label='SSH enabled', initial=True)
+    user = forms.CharField(label='Username', initial='jenkins')
+    home = forms.CharField(label='Home directory', initial='/var/lib/jenkins')
+
+    JenkinsConnections = (
+        ('swarm', 'Swarm plugin'),
+        )
+    jenkins_connection = forms.ChoiceField(label='Jenkins connection', choices=JenkinsConnections, initial='centos6')
--- docker_jenkins_slave/views.py
+++ docker_jenkins_slave/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 from docker_jenkins_slave.forms import DockerfileRequestForm
 
 
 def home(request):
     form = DockerfileRequestForm()
-    return render_to_response("index.html", {'form': form})
+    return render(request, "index.html", {'form': form})
